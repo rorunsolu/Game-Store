e43be90bbcb6814dc0bed6bf5e9529aad83eee99
Ray Orunsolu <rorunsolu@gmail.com>
2024-10-11 17:26:38 +0100

    yh tons of changes, i know i should have made mini commits but yh.....

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 import '/styles/style.scss';
+import '/styles/utilities.scss';
 
 import '/styles/navbar.scss';
 import '/styles/header.scss';
 
+import '/styles/slider-logo.scss';
 import '/styles/platform.scss';
-import '/styles/browse.scss';
 import '/styles/featured.scss';
-import '/styles/looking.scss';
 import '/styles/reason.scss';
+import '/styles/description.scss';
+import '/styles/looking.scss';
+import '/styles/accordion.scss';
 import '/styles/opinions.scss';
 import '/styles/updates.scss';
-import '/styles/newsletter.scss';
 
-import '/styles/utilities.scss';
-import '/styles/accordion.scss';
 import '/styles/footer.scss';
